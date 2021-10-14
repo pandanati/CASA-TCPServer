e0bf1aba11dc00bab5a940738937e3d77f4afa44
pandanati <72080264+pandanati@users.noreply.github.com>
2021-10-14 22:17:14 +0200

    Add project files.

---
+++ CASA-TCPServer/BookCatalog.cs
@@ -0,0 +1,41 @@
+﻿using CASABookClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CASA_TCPServer
+{
+    class BookCatalog
+    {
+        public static readonly List<Book> bookList1 = new List<Book>()
+        {
+            new Book(){Author = "Natalie", ISBN = "123ABC", PageNumber = 5, Title = "Ohh"},
+            new Book(){Author = "Pedro", ISBN = "1234ABCD", PageNumber = 102, Title = "Noo"},
+            new Book(){Author = "Tom", ISBN = "ABCD1234", PageNumber = 105, Title = "The Way Of An Alien"}
+        };
+        
+        public List<Book> GetAll()
+        {
+            List<Book> result = new List<Book>(bookList1);
+            return result;
+        }
+
+        public Book GetByISBN(string isbn)
+        {
+            return bookList1.Find(x => x.ISBN == isbn);
+
+        }
+
+        public void Save(Book newBook)
+        {
+            bookList1.Add(newBook);
+        }
+
+    }
+
+
+
+
+}
---
+++ CASA-TCPServer/TCPBookServer.cs
@@ -0,0 +1,74 @@
+﻿using CASABookClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CASA_TCPServer
+{
+    class TCPBookServer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 4646);
+            listener.Start();
+            Console.WriteLine("The server has now started");
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Task.Run(() => { ManageClient(socket); });
+            }
+        }
+
+        public static void ManageClient(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            BookCatalog catalog = new BookCatalog();
+            Console.WriteLine("The client had now successfully connected");
+
+            while (true)
+            {
+                string firstLine = reader.ReadLine();
+                string secondLine = reader.ReadLine();
+                if (firstLine == "getAll" && string.IsNullOrEmpty(secondLine))
+                {
+                    List<Book> books = catalog.GetAll();
+                    string bookjsonString = JsonSerializer.Serialize(books);
+
+                    writer.WriteLine(bookjsonString);
+                    writer.Flush();
+                }
+
+                if (firstLine == "get" && BookCatalog.bookList1.Any(x => x.ISBN == secondLine))
+                {
+                    Book searchedBook = catalog.GetByISBN(secondLine);
+                    string bookjsonString = JsonSerializer.Serialize(searchedBook);
+
+                    writer.WriteLine(bookjsonString);
+                    writer.Flush();
+                }
+
+                if (firstLine == "Save" && !string.IsNullOrEmpty(secondLine))
+                {
+                    Book inputBook = JsonSerializer.Deserialize<Book>(secondLine);
+                    catalog.Save(inputBook);
+                    writer.WriteLine("Book was successfully added!");
+                    writer.Flush();
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
